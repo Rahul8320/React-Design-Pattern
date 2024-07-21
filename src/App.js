c88c18d79a0339775ece6a6b4f4d89e621b76087
Rahul Dey <rahuldey832000@gmail.com>
2024-07-21 15:06:09 +0530

    Add split component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
+import { SplitComponent } from "./components/SplitComponent";
 import { SplitScreen } from "./components/SplitScreen";
-
-const MyComponent = ({ name, color }) => {
-  return (
-    <div style={{ backgroundColor: color }}>
-      <h1>{name} Component</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati porro
-        eligendi, fugit non minus tempora nihil consequatur cumque hic, itaque
-        quia aliquam et illo sapiente, aut iure officiis veniam cum.
-      </p>
-    </div>
-  );
-};
 
 function App() {
   return (
-    <SplitScreen leftWeight={1} rightWeight={3}>
-      <MyComponent name={"Left"} color={"green"} />
-      <MyComponent name={"Right"} color={"red"} />
+    <SplitScreen leftWeight={4} rightWeight={5}>
+      <SplitComponent name={"Left"} color={"green"} />
+      <SplitComponent name={"Right"} color={"red"} />
     </SplitScreen>
   );
 }
---
+++ src/components/SplitComponent.jsx
@@ -0,0 +1,12 @@
+export const SplitComponent = ({ name, color }) => {
+  return (
+    <div style={{ backgroundColor: color }}>
+      <h1>{name} Component</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati porro
+        eligendi, fugit non minus tempora nihil consequatur cumque hic, itaque
+        quia aliquam et illo sapiente, aut iure officiis veniam cum.
+      </p>
+    </div>
+  );
+};
