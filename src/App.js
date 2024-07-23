8ae4a9eac3086c7ae8e0f5f3c60d203fdc54783e
Rahul Dey <rahuldey832000@gmail.com>
2024-07-23 21:36:02 +0530

    Add numbered list

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import { NumberedList } from "./components/NumberedList";
 import { LargePersonListItem } from "./components/people/LargePersonListItem";
 import { SmallPersonListItem } from "./components/people/SmallPersonListItem";
+import { LargeProductListItem } from "./components/product/LargeProductListItem";
+import { SmallProductListItem } from "./components/product/SmallProductListItem";
 import { RegularList } from "./components/RegularList";
 import { SplitComponent } from "./components/SplitComponent";
 import { SplitScreen } from "./components/SplitScreen";
 import { people } from "./data/people";
+import { products } from "./data/products";
 
 function App() {
   return (
@@ -13,6 +17,7 @@ function App() {
         <SplitComponent name={"Right"} color={"red"} />
       </SplitScreen>
 
+      {/* Regular List  */}
       <RegularList
         items={people}
         itemComponent={SmallPersonListItem}
@@ -23,6 +28,18 @@ function App() {
         items={people}
         itemComponent={LargePersonListItem}
         resourceName={"person"}
+      />
+
+      {/* Numbered List */}
+      <NumberedList
+        items={products}
+        itemComponent={SmallProductListItem}
+        resourceName={"product"}
+      />
+      <NumberedList
+        items={products}
+        itemComponent={LargeProductListItem}
+        resourceName={"product"}
       />
     </>
   );
--- src/components/NumberedList.jsx
+++ src/components/NumberedList.jsx
@@ -1,4 +1,4 @@
-export const RegularList = ({
+export const NumberedList = ({
   items,
   resourceName,
   itemComponent: ItemComponent,
@@ -6,7 +6,10 @@ export const RegularList = ({
   return (
     <div style={{ display: "flex", flexDirection: "column" }}>
       {items.map((item, index) => (
-        <ItemComponent key={index} {...{ [resourceName]: item }} />
+        <>
+          <h3>{index + 1}</h3>
+          <ItemComponent key={index} {...{ [resourceName]: item }} />
+        </>
       ))}
     </div>
   );
